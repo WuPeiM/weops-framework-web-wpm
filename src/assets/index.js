6be5070342c768b75bd0efc464cf5c6ed557a226
qiu <qiujiaxp@163.com>
2023-03-09 10:22:26 +0800

    feature: init

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,4 @@
 // 样式统一引入
 import './base/css/base.scss'
-import './base/css/patch.scss'
-import './base/css/color.scss'
 import './custom/css/common.scss'
 import './base/font/new-bk-icon/iconfont.css'
