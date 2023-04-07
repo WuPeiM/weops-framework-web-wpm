5a2c1a858aac0f16bf8c8004f9f5b8dde305ee10
qiu <qiujiaxp@163.com>
2023-04-07 18:59:13 +0800

    feature: 优化菜单展示

--- src/router/frameRouter.ts
+++ src/router/frameRouter.ts
@@ -1,7 +1,7 @@
 let mainRouter = []
 let routeConfig = []
 let subsMenuPromission = {}
-
+let manageMenu = []
 // 自动导入子应用
 // @ts-ignore
 const files = require.context('@/projects', true, /frameRouter.ts/)
@@ -12,15 +12,17 @@ files.keys().forEach(key => {
     mainRouter = mainRouter.concat(router)
     routeConfig = routeConfig.concat(menuList).sort((a, b) => a.sortIndex - b.sortIndex)
     subsMenuPromission = { ...subsMenuPromission, ...subsMenu }
-    routeConfig.forEach(item => {
-        if (item.id === 'Setting') {
-            const manageMenu = files(key).manageMenu
-            if (manageMenu) {
-                item.children = item.children.concat(manageMenu).sort((a, b) => a.sortIndex - b.sortIndex)
-            }
-        }
-    })
+    if (files(key).manageMenu) {
+        manageMenu = manageMenu.concat(files(key).manageMenu)
+    }
 })
+
+routeConfig.forEach(item => {
+    if (item.id === 'Setting') {
+        item.children = item.children.concat(manageMenu).sort((a, b) => a.sortIndex - b.sortIndex)
+    }
+})
+
 export const frameRouter = mainRouter
 
 export const menuList = routeConfig
