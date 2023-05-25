f1b3d2a2586f66a029a8efb98dcaf6fa9ae948eb
CharlesWhj <1253297146@qq.com>
2023-05-25 17:36:52 +0800

    feature:服务台管理拆分菜单、非超管不能进入服务台管理相关菜单

--- src/router/index.ts
+++ src/router/index.ts
@@ -63,12 +63,17 @@ const handleRouteAuthorization = async(to, from, next) => {
 }
 
 function checkRouteAccess(to, from, next) {
+    const permission = store.state.permission
     // 远程连接不做路由拦截
     if (to.name === 'RemoteConnect') {
         next()
         return
     }
-    const permission = store.state.permission
+    // 普通用户不能进入服务台管理
+    if (!permission.user.is_super && to?.meta?.relatedMenu === 'ServiceDeskManage') {
+        next({path: from.path})
+        return
+    }
     const isDefinedRoute = frameRouter.some(item => item.name === to?.name)
     const ids = findIdsWithNoChildren(permission.menuList).concat(['404', '403', 'AuthPermissionFail'])
     const isHasPermission = ids.includes(to.name) || (to?.meta?.parentIds || []).filter(r => ids.includes(r)).length || ids.includes(to?.meta?.relatedMenu)
@@ -95,9 +100,6 @@ function dealRouterByPermission(to, from, next) {
         } else {
             next({name: 'CreditManage'})
         }
-    }
-    if (!userInfo.is_super && to.name === 'ServiceDeskManage') {
-        next({path: from.path})
     }
     if (to.name === 'AuthPermissionFail' && menuList.length === 0) {
         next()
