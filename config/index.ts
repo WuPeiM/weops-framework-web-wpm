ff87bd628daa6a8ba6199b996331b7ef4984fe0f
李娟虹[hollyli] <hollyli@canway.net>
2023-07-13 15:32:42 +0800

    feature: 引入plotly.js

--- config/index.ts
+++ config/index.ts
@@ -12,7 +12,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-                target: '', // 设置你调用的接口域名和端口号 别忘了加http
+                target: 'http://paas.weops.com/o/weops_saas/#/', // 设置你调用的接口域名和端口号 别忘了加http
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''// 这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
@@ -21,7 +21,7 @@ module.exports = {
         },
 
         // Various Dev Server settings
-        host: 'localhost', // can be overwritten by process.env.HOSTcardList
+        host: 'dev.weops.com', // can be overwritten by process.env.HOSTcardList
         port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: true,
         errorOverlay: true,
