f10561a5e0327dd3e40226b7d4197087f717ffa2
CharlesWhj <1253297146@qq.com>
2023-10-30 15:59:08 +0800

    feature: 角色管理添加mock示例

---
+++ src/api/module/mock/roleManageMock.ts
@@ -0,0 +1,107 @@
+// 角色管理模块
+import {get} from '@/api/axiosconfig/axiosconfig'
+import Mock from 'mockjs'
+export default {
+    /**
+     * 获取角色列表数据
+     *
+     * @param {Object} params 请求参数
+     */
+    getRoleList(params = {}) {
+        Mock.mock(/api\/system\/mgmt\/role_manage\/get_roles/, 'get', {
+            result: true,
+            code: '20000',
+            message: 'success',
+            data: {
+                count: 3,
+                items: [
+                    {
+                        built_in: true,
+                        created_at: '2023-08-28 23:45:47',
+                        describe: '本角色为IT服务台工单管理员，拥有查看所有工单的权限',
+                        id: 5,
+                        is_super: false,
+                        role_name: 'itsm_admin_group',
+                        users: [
+                            {
+                                bk_user_id: 3,
+                                bk_username: 'pwm',
+                                chname: '狗蛋',
+                                id: 7
+                            },
+                            {
+                                bk_user_id: 8,
+                                bk_username: 'anneyan',
+                                chname: '颜缨',
+                                id: 13
+                            },
+                            {
+                                bk_user_id: 15,
+                                bk_username: 'hunter82',
+                                chname: 'hunter82',
+                                id: 19
+                            }
+                        ]
+                    },
+                    {
+                        built_in: true,
+                        created_at: '2023-08-28 23:45:47',
+                        describe: '本角色为超级管理员，有全部的权限',
+                        id: 4,
+                        is_super: true,
+                        role_name: 'iadmin_group',
+                        users: [
+                            {
+                                bk_user_id: 1,
+                                bk_username: 'admin',
+                                chname: '超管',
+                                id: 2
+                            },
+                            {
+                                bk_user_id: 3,
+                                bk_username: 'pwm',
+                                chname: '狗蛋',
+                                id: 7
+                            },
+                            {
+                                bk_user_id: 4,
+                                bk_username: 'eric',
+                                chname: 'Eric',
+                                id: 8
+                            },
+                            {
+                                bk_user_id: 7,
+                                bk_username: 'kayla',
+                                chname: 'kayla',
+                                id: 12
+                            }
+                        ]
+                    },
+                    {
+                        built_in: true,
+                        created_at: '2023-08-28 23:45:47',
+                        describe: '本角色为普通用户，需要超级管理员赋予其他权限',
+                        id: 3,
+                        is_super: false,
+                        role_name: 'normal_group',
+                        users: [
+                            {
+                                bk_user_id: 4,
+                                bk_username: 'eric',
+                                chname: 'Eric',
+                                id: 8
+                            },
+                            {
+                                bk_user_id: 14,
+                                bk_username: 'chadneal',
+                                chname: 'chadneal',
+                                id: 18
+                            }
+                        ]
+                    }
+                ]
+            }
+        })
+        return get('/system/mgmt/role_manage/get_roles/', params)
+    }
+}
