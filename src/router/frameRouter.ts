e035c9b2414cf75f2da29eac0d9428da10da90db
吴培铭 <1780439574@qq.com>
2023-12-13 10:40:09 +0800

    minor: 取消自动导入子应用注释

--- src/router/frameRouter.ts
+++ src/router/frameRouter.ts
@@ -269,35 +269,35 @@ const manageMenu = []
 
 // 自动导入子应用
 // @ts-ignore
-// const files = require.context('@/projects', true, /frameRouter.ts/)
-// files.keys().forEach(key => {
-//     const router = files(key).frameRouter
-//     const menuList = files(key).adminRouteConfig
-//     const subsMenu = files(key).subsMenuList
-//     mainRouter = mainRouter.concat(router)
-//     // 处理合并项目菜单
-//     menuList.forEach(item => {
-//         const targetIndex = routeConfig.findIndex(tex => tex.id === item.id)
-//         if (targetIndex !== -1) {
-//             routeConfig[targetIndex].children = routeConfig[targetIndex].children.concat(item.children).sort((a, b) => a.sortIndex - b.sortIndex)
-//         } else {
-//             routeConfig.push(item)
-//         }
-//     })
-//     routeConfig.sort((a, b) => a.sortIndex - b.sortIndex)
-//     subsMenuPromission = { ...subsMenuPromission, ...subsMenu }
-//     const manageMenuItem = files(key).manageMenu
-//     if (manageMenuItem) {
-//         manageMenuItem.forEach(item => {
-//             const findIndex = manageMenu.findIndex(tex => tex.id === item.id)
-//             if (findIndex !== -1) {
-//                 manageMenu[findIndex].children = manageMenu[findIndex].children.concat(item.children).sort((a, b) => a.sortIndex - b.sortIndex)
-//             } else {
-//                 manageMenu.push(item)
-//             }
-//         })
-//     }
-// })
+const files = require.context('@/projects', true, /frameRouter.ts/)
+files.keys().forEach(key => {
+    const router = files(key).frameRouter
+    const menuList = files(key).adminRouteConfig
+    const subsMenu = files(key).subsMenuList
+    mainRouter = mainRouter.concat(router)
+    // 处理合并项目菜单
+    menuList.forEach(item => {
+        const targetIndex = routeConfig.findIndex(tex => tex.id === item.id)
+        if (targetIndex !== -1) {
+            routeConfig[targetIndex].children = routeConfig[targetIndex].children.concat(item.children).sort((a, b) => a.sortIndex - b.sortIndex)
+        } else {
+            routeConfig.push(item)
+        }
+    })
+    routeConfig.sort((a, b) => a.sortIndex - b.sortIndex)
+    subsMenuPromission = { ...subsMenuPromission, ...subsMenu }
+    const manageMenuItem = files(key).manageMenu
+    if (manageMenuItem) {
+        manageMenuItem.forEach(item => {
+            const findIndex = manageMenu.findIndex(tex => tex.id === item.id)
+            if (findIndex !== -1) {
+                manageMenu[findIndex].children = manageMenu[findIndex].children.concat(item.children).sort((a, b) => a.sortIndex - b.sortIndex)
+            } else {
+                manageMenu.push(item)
+            }
+        })
+    }
+})
 routeConfig.forEach(item => {
     if (item.id === 'Setting') {
         item.children = item.children.concat(manageMenu).sort((a, b) => a.sortIndex - b.sortIndex)
